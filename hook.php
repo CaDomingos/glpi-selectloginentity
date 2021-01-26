7876b73513b6b9e12ba0100e9545c5ee5af526d5
CaDomingos <cleberanderson@msn.com>
2021-01-26 03:40:46 -0400

    Update hook.php

--- hook.php
+++ hook.php
@@ -2,8 +2,7 @@
 
 /*
  *
-
-  This file is part of the Modifications plugin.
+ This file is part of the Modifications plugin.
 
  Order plugin is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
